417452e64c86f729689a9c43fa7cef02e84d2377
Богдан Ільніцький <bdancik8@gmail.com>
2024-06-21 12:00:18 +0300

    struct

--- Project2/9_1_7.c
+++ Project2/9_1_7.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#pragma warning(disable:4996)
+
+#define MAT_POSDEF 1
+#define MAT_POSSEMDEF 2
+#define MAT_NOTPOSDEF 0
+
+typedef struct {
+    int rows;
+    int cols;
+    double** data;
+} MAT;
+
+
+int main() {
+    int size;
+    printf("¬ведите размер n матрицы n x 2: ");
+    scanf("%d", &size);
+
+}
