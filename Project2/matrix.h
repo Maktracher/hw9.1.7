5daac0b5850f26c36217623c369a389c266c3d4c
Богдан Ільніцький <bdancik8@gmail.com>
2024-06-25 13:25:14 +0300

    matrix.h void char

--- Project2/matrix.h
+++ Project2/matrix.h
@@ -10,3 +10,11 @@ typedef struct {
 #define MAT_POSDEF 1
 #define MAT_POSSEMDEF 2
 #define MAT_NOTPOSDEF 0
+
+void create_matrix(MAT* mat, int rows, int cols);
+void free_matrix(MAT* mat);
+void print_matrix(MAT* mat);
+void populate_matrix_auto(MAT* mat);
+char mat_test_positive_definiteness(MAT* mat);
+
+#endif
