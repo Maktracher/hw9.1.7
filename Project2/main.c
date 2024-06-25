a4b663db8b227bda5a6405e517bde2c03c606551
Богдан Ільніцький <bdancik8@gmail.com>
2024-06-25 13:37:25 +0300

    main. exe

--- Project2/main.c
+++ Project2/main.c
@@ -1,10 +1,19 @@
-﻿// Main function
+﻿#include <stdio.h>
+#include "matrix.h"
+
+void clear_input_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
 int main() {
     int rows, cols;
     printf("Enter the number of rows for the matrix: ");
     scanf("%d", &rows);
+    clear_input_buffer();
     printf("Enter the number of columns for the matrix: ");
     scanf("%d", &cols);
+    clear_input_buffer();
 
     MAT mat;
     create_matrix(&mat, rows, cols);
@@ -22,6 +31,9 @@ int main() {
         printf("The matrix is not positive definite\n");
 
     free_matrix(&mat);
+
+    printf("Stlačte Enter pre pokračovanie...");
+    getchar();  
 
     return 0;
 }
